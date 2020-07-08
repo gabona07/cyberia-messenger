9e70e6347e93937941f606f5550aebed36ab2c3c
Gabriella Bona <bonagabriella1995@gmail.com>
2020-07-08 10:17:13 +0200

    Adjust layout

--- app/src/main/java/com/codecool/cyberia/MainActivity.kt
+++ app/src/main/java/com/codecool/cyberia/MainActivity.kt
@@ -2,11 +2,15 @@ package com.codecool.cyberia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = email_registration.text.toString()
+        val password = password_registration.text.toString()
     }
 }
