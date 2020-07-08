fc4454c8d92e214d0582eeb92b25cfdfe3eb8f99
Gabriella Bona <bonagabriella1995@gmail.com>
2020-07-08 10:56:23 +0200

    Add login layout changes

--- app/src/main/java/com/codecool/cyberia/LoginActivity.kt
+++ app/src/main/java/com/codecool/cyberia/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.codecool.cyberia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        back_to_registration.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
