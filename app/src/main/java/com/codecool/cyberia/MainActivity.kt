550cbe609701dba992c45dcabfa30f45432ffab0
Gabriella Bona <bonagabriella1995@gmail.com>
2020-07-08 10:46:14 +0200

    Add login activity

--- app/src/main/java/com/codecool/cyberia/MainActivity.kt
+++ app/src/main/java/com/codecool/cyberia/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.codecool.cyberia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,7 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = email_registration.text.toString()
-        val password = password_registration.text.toString()
+
+
+        register_button.setOnClickListener {
+            val email = email_registration.text.toString()
+            val password = password_registration.text.toString()
+        }
+
+        have_account.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
